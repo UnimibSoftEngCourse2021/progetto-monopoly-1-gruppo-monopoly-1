f981ef0139fe9c4b3608e52323b0114152a1d231
GIULIO RIGGIO <72838014+GiulioRiggio@users.noreply.github.com>
2021-01-06 01:10:09 +0100

    Update ComponentController.js

--- src/components/ComponentController.js
+++ src/components/ComponentController.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
+var primoMsgTA= 'ultimo messaggio';
+var secondoMsgTA= 'penultimo messaggio';
+var terzoMsgTA= 'terzultimo messaggio';
+
+function     funz2() {
+    this.setState({messaggioUno: 'dddsj'})
+
+    //this.messaggioUno = str1
+}
+
+function funzprova() {
+    //alert('prova ');   // ok
+    //funz2();
+    //alert(document.getElementById('w3review').value);   // ok
+    primoMsgTA= 'ddddddddddd';
+}
+
 class ComponentController extends React.Component {
 
 	constructor(props) {
         super(props);
         this.state = {
-            messaggioTA1: "ultimo messaggio",
-            messaggioTA2: "penultimo messaggio",
-            messaggioTA3: "terzultimo messaggio"
+
         };
       }
+
 
     render () {
 
@@ -17,11 +33,11 @@ class ComponentController extends React.Component {
             <table className="tableController">
             <tr className="trController">
                 <td className="tdController">
-                    <button type="button" onClick={() => alert('tira dadi')}>Tira dadi</button></td>
+                    <button onClick={() => funzprova()}>Tira dadi</button></td>
                 <td className="tdController">
                     <button type="button" onClick={() => alert('costruisci')}>Costruisci</button></td>
                 <td className="tdController">
-                    <button type="button" onClick={() => alert('vendi')}>Vendi</button></td>    
+                    <button type="button" onClick={() => alert('vendi')}>Vendi {primoMsgTA}</button></td>    
                 <td className="tdController">
                     <button type="button" onClick={() => alert('fine turno')}>Fine turno</button></td>   
                 <td className="tdController">
@@ -29,15 +45,45 @@ class ComponentController extends React.Component {
             </tr>
             <tr className="trControllerTA">
                 <td className="tdController" colspan="5">
-                    <textarea className="ControllerTA" id="w3review" name="w3review" >
-                     {this.state.messaggioTA1+"\n"+this.state.messaggioTA2+"\n"+this.state.messaggioTA3}
-                    </textarea>
+                    <AreaTesto />
                 </td>  
             </tr>
             </table>            
         )
     }
+
+
 }
+
+// {this.state.messaggioTA1+"\n"+this.state.messaggioTA2+"\n"+this.state.messaggioTA3}
+// https://it.reactjs.org/docs/lifting-state-up.html
+
+class AreaTesto extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        messaggioUno: primoMsgTA,
+        messaggioDue: secondoMsgTA,
+        messaggioTre: terzoMsgTA
+      };
+    }
+  
+    render() {
+      return (
+        <textarea className="ControllerTA" id="w3review" name="w3review" >
+            {this.state.messaggioUno+"\n"+this.state.messaggioDue+"\n"+this.state.messaggioTre}
+        </textarea>
+      );
+    }
+
+    funz1() {
+        this.setState({messaggioUno: 'dddsj'})
+
+        //this.messaggioUno = str1
+    }
+
+  }
+
 
 export default ComponentController;
 
