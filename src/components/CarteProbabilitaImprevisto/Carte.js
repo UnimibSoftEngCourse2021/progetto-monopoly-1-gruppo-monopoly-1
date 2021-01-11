31d0ad22e57f448f28932d552d1ff623d2aaaf9b
Alessandro <a.rosa@campus.unimib.it>
2021-01-12 00:06:00 +0100

    Aggiunto file Carte.js contenente la prima bozza delle carte Probabilità o Imprevisto

---
+++ src/components/CarteProbabilitaImprevisto/Carte.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Banca from '../Banca';
+
+class Carte extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //carta = [Numero carta, Descrizione, Somma di denaro, Somma per ogni casa, Somma per ogni albergo, Somma per ogni giocatore]
+            carte: [
+                //Probabilità
+                [0, 'Serata della Grande Opera: incassa $50 da ogni giocaotore', 0, 0, 0, 50],
+                [1, 'Errore della banca: incassa $200', 200, 0, 0, 0],
+                [2, 'Parcella del medico: paga $50', -50, 0, 0, 0],
+                [3, 'Rimborso della imposta sul reddito: incassa $20', 20, 0, 0, 0],
+                [4, 'Rendimento assicurazione sulla vita: incassa $100', 100, 0, 0, 0],
+                [5, 'Paga le spese mediche ospedaliere: paga $50', -50, 0, 0, 0],
+                [6, 'Paga le tasse scolastiche: paga $50', -50, 0, 0, 0],
+                [7, 'Commisione consulenza: incassa $25', 25, 0, 0, 0],
+                [8, 'Hai vinto il secondo posto in una gara di bellezza: incassa $10', 10, 0, 0, 0],
+                [9, 'Eredità: incassa $100', 100, 0, 0, 0],
+                [10, 'Vendita di azioni: incassa $100', 100, 0, 0, 0],
+                [11, 'Rendimento fondo vacanze: incassa $100', 100, 0, 0, 0],
+                [12, 'Sei stato tassato per le riparazioni in questa strada: paga $45 per casa e $115 per hotel', 0, -45, -115, 0],
+                [13, 'Esci di Prigione', 0, 0, 0, 0],
+                [14, 'Vai in Prigione senza passare dal Via', 0, 0, 0, 0],
+                [15, 'Vai al Via', 0, 0, 0, 0], //ATTENZIONE LA CARTA DOVREBBE DARE 200$, DA FARE DOPO SVILUPPO DELLA CASELLA VIA
+
+                //Imprevisti
+                [16, 'Esci di Prigione', 0, 0, 0, 0],
+                [17, 'Vai al Via', 0, 0, 0, 0], //ATTENZIONE LA CARTA DOVREBBE DARE 200$, DA FARE DOPO SVILUPPO DELLA CASELLA VIA
+                [18, 'Vai a Illinois Avenue', 0, 0, 0, 0], //ATTENZIONE LA CARTA DOVREBBE DARE 200$ IN CASO SI PASSI DAL VIA, DA FARE DOPO SVILUPPO DELLA CASELLA VIA
+                [19, 'Vai a St. Charles Place', 0, 0, 0, 0], //ATTENZIONE LA CARTA DOVREBBE DARE 200$ IN CASO SI PASSI DAL VIA, DA FARE DOPO SVILUPPO DELLA CASELLA VIA
+                [20, 'La Banca paga un dividendo: incassi $50', 50, 0, 0, 0],
+                [21, 'Vai indietro di 3 caselle', 0, 0, 0, 0],
+                [22, 'Vai in Prigione senza passare dal Via', 0, 0, 0, 0],
+                [23, 'Riparazioni generali a tutte le tue proprietà: paga $25 per casa e $100 per hotel', 0, -25, -100, 0],
+                [24, 'Paga tasse scadute', -10, 0, 0, 0],
+                [25, 'Vai a Reading Railroad', 0, 0, 0, 0],
+                [26, 'Vai a Boardwalk', 0, 0, 0, 0],
+                [27, 'Sei eletto chairman della tavola da gioco: paga $50 da ogni giocaotore', 0, 0, 0, -50],
+                [28, 'Rendimento prestito immobiliare: incassa $150', 150, 0, 0, 0],
+                [29, 'Muovi la pedina al prossimo Utility, se non ha proprietario puoi comprarlo, se lo ha tira i dadi e paga 10 volte il risultato del lancio', 0, 0, 0, 0],
+                [30, 'Muovi la pedina alla prossimo Stazione, se non ha proprietario puoi comprarla, se lo ha paga il doppio del noleggio', 0, 0, 0, 0],
+                [31, 'Hai vinto una competizione di cruciverba: incassa $100', 100, 0, 0, 0],
+
+            ]
+        }
+    }
+
+
+    
+    estraiCarta = (probabilitaOImprevisto, giocatore) => { //probabilità==true imprevisto==false
+        if (probabilitaOImprevisto){
+            idCarta = Math.floor(Math.random()*(15));
+        }else{
+            idCarta = Math.floor(Math.random()*(31-16)+16);
+        };
+        this.props.attivaCarta(idCarta, giocatore);
+    }
+
+    
+    attivaCarta = (idCarta, giocatore) => {
+        //  TODO
+    }
+}
+export default Carte;
