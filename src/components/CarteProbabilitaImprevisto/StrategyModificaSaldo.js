abd3c4d757dd896bcb3c4fbedd0e7e697e7f89e5
Raffaele Cerizza <r.cerizza1@campus.unimib.it>
2021-01-28 01:29:30 +0100

    Starting Strategy pattern for cards handling

---
+++ src/components/CarteProbabilitaImprevisto/StrategyModificaSaldo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class StrategyModificaSaldo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default StrategyModificaSaldo;
---
+++ src/components/CarteProbabilitaImprevisto/StrategySpostaADestinazione.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class StrategySpostaADestinazione extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default StrategySpostaADestinazione;
